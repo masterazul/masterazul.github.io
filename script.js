951e559c4bd63dabce9fd4bc37b1db55d718495d
Masterazul <ae_augusto33@proton.me>
2024-06-10 00:24:45 -0300

    teste7

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-  function flipPageAndRedirect(url) {
-    document.body.classList.add('page-flip');
-    setTimeout(function() {
-      window.location.href = url;
-    }, 500); // Tempo em milissegundos que corresponde à duração da animação
-  }
-</script>
